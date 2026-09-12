b41b31d0f82e7ff3b39f0b91744f65f7cbbb2eb6
John Han <work.johnhan@gmail.com>
2026-09-12 17:07:47 +0900

    build(android): 릴리스 코드 및 리소스 축소 활성화

--- app.config.js
+++ app.config.js
@@ -155,6 +155,8 @@ module.exports = {
             extraProguardRules:
               "-keep class com.google.android.gms.internal.consent_sdk.** { *; }",
             kotlinVersion: "2.1.20",
+            enableMinifyInReleaseBuilds: true,
+            enableShrinkResourcesInReleaseBuilds: true,
           },
         },
       ],
